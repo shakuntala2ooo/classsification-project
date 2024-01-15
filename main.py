795af5044bee8531f6cbe4e36d540d56f780f57d
Shakuntala Sarkar <112191429+shakuntala2ooo@users.noreply.github.com>
2024-01-16 01:02:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import cv2
 
 cap=cv2.VideoCapture(0)
 myClassifier=cvzone.Classifier('keras_model/keras_model.h5','labels.txt')
+
+fpsReader=cvzone.FPS()
 while True:
     _, img=cap.read()
-    predictions = myClassifier.getPrediction(img)
+    predictions = myClassifier.getPrediction(img, scale=1.5)
+    #print(predictions, index)
+    fps, img =fpsReader.update(img, pos=(450,50))
     
     cv2.imshow("Image",img)
     cv2.waitKey(1)
